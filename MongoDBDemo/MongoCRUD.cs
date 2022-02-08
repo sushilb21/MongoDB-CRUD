fa9764cefd0e25aea563caf75f2e13224773784b
Sushil Bhingardive <sushilb@versionsolutions.com>
2022-02-09 00:18:40 +0530

    Add project files.

---
+++ MongoDBDemo/MongoCRUD.cs
@@ -0,0 +1,87 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+
+public class MongoCRUD
+{
+    private IMongoDatabase _database;
+
+    public MongoCRUD(string database)
+    {
+        var client = new MongoClient();
+        _database = client.GetDatabase(database);
+    }
+
+    public class NameModel
+    {
+        [BsonId]
+        public Guid Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+
+    public class PersonModel
+    {
+        [BsonId]
+        public Guid Id { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public AddressModel PrimaryAddress { get; set; }
+
+        [BsonElement("dob")]
+        public DateTime DateOfBirth { get; set; }
+    }
+
+
+    public class AddressModel
+    {
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+    }
+
+    public void InsertRecord<T>(string table, T record)
+    {
+        var colllection = _database.GetCollection<T>(table);
+        colllection.InsertOne(record);
+    }
+
+    public List<T> LoadRecords<T>(string table)
+    { 
+        var collection = _database.GetCollection<T>(table);
+
+        return collection.Find(new BsonDocument()).ToList();
+    }
+
+    public T LoadRecordById<T>(string table, Guid id) 
+    { 
+        var collection = _database.GetCollection<T>(table);
+
+        var filter = Builders<T>.Filter.Eq("Id", id);
+
+        return collection.Find(filter).First();
+    
+    }
+
+    public void UpsertRecord<T>(string table, Guid id, T record)
+    {
+        var collection = _database.GetCollection<T>(table);
+
+        var result = collection.ReplaceOne(new BsonDocument("_id",id), record, new ReplaceOptions { IsUpsert = true});
+    }
+
+    public void DeleteRecord<T>(string table, Guid id)
+    { 
+        var collection = _database.GetCollection<T>(table);
+
+        var filter = Builders<T>.Filter.Eq("Id", id);
+
+        collection.DeleteOne(filter);
+    }
+}
---
+++ MongoDBDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿using static MongoCRUD;
+
+MongoCRUD db = new MongoCRUD("AddressBook");
+
+var personModel = new PersonModel
+{
+    FirstName = "Sushil",
+    LastName = "B",
+
+    PrimaryAddress = new AddressModel
+    {
+        StreetAddress = "Test1",
+        City = "Test2",
+        State = "Test4",
+        ZipCode = "411057"
+    }
+};
+
+db.InsertRecord("Users", personModel);
+
+var records1 = db.LoadRecords<PersonModel>("Users");
+
+foreach (var rec in records1)
+{
+    Console.WriteLine($"{rec.Id }: {rec.FirstName} {rec.LastName}");
+
+    if (rec.PrimaryAddress != null)
+    {
+        Console.WriteLine($"{rec.PrimaryAddress.City }");
+    }
+}
+
+var records = db.LoadRecords<PersonModel>("Users");
+var guid = records?.LastOrDefault()?.Id;
+
+var oneRec = db.LoadRecordById<PersonModel>("Users", guid.Value);
+
+oneRec.DateOfBirth = DateTime.UtcNow;
+
+db.UpsertRecord("Users", oneRec.Id, oneRec);
+
+var oneRec1 = db.LoadRecordById<PersonModel>("Users", guid.Value);
+
+db.DeleteRecord<PersonModel>("Users", oneRec.Id);
+
+Console.WriteLine($"{oneRec.FirstName} {oneRec.LastName}");
+
+Console.ReadLine();
